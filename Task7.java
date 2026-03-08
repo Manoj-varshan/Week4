1ebea00b3ade64581340ba77a75bcfbea5d5a4c5
dev-varshan <manojvarshan374@gmail.com>
2026-03-08 12:26:37 +0530

    task7

--- Task7.java
+++ Task7.java
@@ -1,15 +1,33 @@
+$repoPath = "C:\path\to\your\project"
+$counterFile = "$repoPath\task_counter.txt"
 
-
-class Result {
-    public static int diagonalDifference(List<List<Integer>> arr) {
-        int n = arr.size();
-        int d1 = 0, d2 = 0;
-
-        for (int i = 0; i < n; i++) {
-            d1 += arr.get(i).get(i);
-            d2 += arr.get(i).get(n - 1 - i);
-        }
-
-        return Math.abs(d1 - d2);
-    }
+if (!(Test-Path $counterFile)) {
+    "1" | Out-File $counterFile
 }
+
+$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.Path = $repoPath
+$watcher.Filter = "*.*"
+$watcher.IncludeSubdirectories = $true
+$watcher.EnableRaisingEvents = $true
+
+Register-ObjectEvent $watcher Created -Action {
+
+    $repoPath = "C:\Users\deepi\Documents\Placement Training\PSTJ\Func\Week4"
+    $counterFile = "$repoPath\task_counter.txt"
+
+    $taskNumber = Get-Content $counterFile
+    $message = "task$taskNumber"
+
+    cd $repoPath
+
+    git add .
+    git reset git-autopush.ps1
+    git commit -m $message
+    git push origin main
+
+    $taskNumber = [int]$taskNumber + 1
+    $taskNumber | Out-File $counterFile
+}
+
+while ($true) { Start-Sleep 1 }
